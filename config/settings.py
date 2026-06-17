f15c9d830b56c164cf39d5720b97511db2d32b80
xavier4-csr <luvaidavid3@gmail.com>
2026-06-17 22:51:03 +0300

    login debug

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
